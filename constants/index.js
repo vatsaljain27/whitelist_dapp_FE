8770793fc726020759f66963d820d8e602ff2004
Vatsal Jain <vatsal@Vatsals-MacBook-Air.local>
2023-05-04 01:02:56 -0700

    second commit

---
+++ constants/index.js
@@ -0,0 +1,68 @@
+export const WHITELIST_CONTRACT_ADDRESS =
+  "0x22A866da57e01D1D8c183F27a5857d4AdCF6e532";
+
+export const abi = [
+  {
+    inputs: [
+      {
+        internalType: "uint8",
+        name: "_maxWhitelistedAddresses",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [],
+    name: "addAddressToWhitelist",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "maxWhitelistedAddresses",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "numAddressesWhitelisted",
+    outputs: [
+      {
+        internalType: "uint8",
+        name: "",
+        type: "uint8",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    name: "whitelistedAddresses",
+    outputs: [
+      {
+        internalType: "bool",
+        name: "",
+        type: "bool",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+];
---
+++ pages/index.js
@@ -0,0 +1,155 @@
+import Head from "next/head";
+// import Image from 'next/image'
+import styles from "@/styles/Home.module.css";
+import { useState, useEffect, useRef } from "react";
+import Web3Modal from "web3modal";
+import { providers, Contract } from "ethers";
+import { WHITELIST_CONTRACT_ADDRESS, abi } from "@/constants";
+
+export default function Home() {
+  const [walletConnected, setWalletConnected] = useState(false);
+  const [numOfWhitelisted, setNumOfWhitelisted] = useState(0);
+  const [joinedWhitelist, setJoinWhitelist] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const web3ModalRef = useRef();
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    try {
+      const provider = await web3ModalRef.current.connect();
+      const web3Provider = new providers.Web3Provider(provider);
+      const { chainId } = await web3Provider.getNetwork();
+      if (chainId !== 5) {
+        window.alert("Change the network to Goerli");
+        throw new Error("Change the network to Goerli");
+      }
+      if (needSigner) {
+        const signer = web3Provider.getSigner();
+        return signer;
+      }
+      return web3Provider;
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const addAddressToWhitelist = async () => {
+    try {
+      const signer = await getProviderOrSigner(true);
+      const whitelistContract = new Contract(
+        WHITELIST_CONTRACT_ADDRESS,
+        abi,
+        signer
+      );
+      const tx = await whitelistContract.addAddressToWhitelist();
+      setLoading(true);
+      await tx.wait();
+      setLoading(false);
+      await getNumberOfWhitelisted();
+      setJoinWhitelist(true);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const checkIfAddressIsWhitelisted = async () => {
+    try {
+      const signer = getProviderOrSigner(true);
+      const whitelistContract = new Contract(
+        WHITELIST_CONTRACT_ADDRESS,
+        abi,
+        signer
+      );
+      const address = await signer.getAddress();
+      const _joinedWhitelist = await whitelistContract.whitelistedAddresses(
+        address
+      );
+      setJoinWhitelist(_joinedWhitelist);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const getNumberOfWhitelisted = async () => {
+    try {
+      const provider = await getProviderOrSigner();
+      const whitelistContract = new Contract(
+        WHITELIST_CONTRACT_ADDRESS,
+        abi,
+        provider
+      );
+      const _numOfWhitelisted =
+        await whitelistContract.numAddressesWhitelisted();
+      setNumOfWhitelisted(_numOfWhitelisted);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const renderButton = () => {
+    if (walletConnected) {
+      if (joinedWhitelist) {
+        return (
+          <div className={styles.description}>
+            Thanks for Joining the Whitelist!
+          </div>
+        );
+      } else if (loading) {
+        return <button className={styles.button}>Loading...</button>;
+      } else {
+        return (
+          <button onClick={addAddressToWhitelist} className={styles.button}>
+            Join the Whitelist
+          </button>
+        );
+      }
+    } else {
+      return (
+        <button onClick={connectWallet} className={styles.button}>
+          Connect Wallet
+        </button>
+      );
+    }
+  };
+
+  const connectWallet = async () => {
+    try {
+      await getProviderOrSigner();
+      setWalletConnected(true);
+      checkIfAddressIsWhitelisted();
+      getNumberOfWhitelisted();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    if (!walletConnected) {
+      web3ModalRef.current = new Web3Modal({
+        network: "goerli",
+        providerOptions: {},
+        disabledInjectedProvider: false,
+      });
+      connectWallet();
+    }
+  }, [walletConnected]);
+  return (
+    <div>
+      <Head>
+        <title>Whitelist dApp</title>
+        <meta name="description" content="Whitelist-Dapp" />
+      </Head>
+      <div className={styles.main}>
+        <h1 className={styles.title}>Welcome to Crypto Devs!</h1>
+        <div className={styles.description}>
+          {numOfWhitelisted} have already joined the Whitelist.
+        </div>
+        {renderButton()}
+        <div>
+          <img className={styles.image} src="./crypto-devs.svg" />
+        </div>
+      </div>
+      <footer className={styles.footer}>Made with &#9829; by CryptoDevs</footer>
+    </div>
+  );
+}
